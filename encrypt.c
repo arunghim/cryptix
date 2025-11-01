3d140e5a64e176e39b0d960b66710a4e916dc79c
Arun Ghimire <dev.arunghimire@gmail.com>
2025-11-01 19:45:34 -0400

    Update: Encryption Algorithm & Printable ASCII Characters

---
+++ encrypt.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define BLOCK_SIZE 8
+
+void encrypt(FILE *in, FILE *out) {
+    unsigned char block[BLOCK_SIZE];
+    size_t n;
+
+    while ((n = fread(block, 1, BLOCK_SIZE, in)) > 0) {
+        if (n < BLOCK_SIZE) {
+            for (size_t i = 0; i < n; ++i)
+                fputc((block[i] % 95) + 32, out);
+            break;
+        }
+
+        unsigned char enc[BLOCK_SIZE - 1];
+        unsigned char src = block[7];
+
+        int h[8];
+        for (int i = 1; i <= 7; ++i)
+            h[i] = (src >> (7 - i)) & 1;
+
+        for (int i = 0; i < 7; ++i) {
+            int bitPos = 7 - i;
+            int bitVal = h[7 - i];
+            unsigned char b = block[i];
+            unsigned char result = 0;
+
+            for (int bit = 7; bit > bitPos; --bit)
+                result |= ((b >> (bit - 1)) & 1) << bit;
+            result |= (bitVal << bitPos);
+            for (int bit = bitPos - 1; bit >= 0; --bit)
+                result |= ((b >> bit) & 1) << bit;
+
+            enc[i] = (result % 95) + 32;
+        }
+
+        fwrite(enc, 1, 7, out);
+    }
+}
--- main.c
+++ main.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void encrypt(FILE *in, FILE *out);
+
 int main(int argc, char *argv[]) {
-    if (argc != 2) return 1;
+    if (argc != 3) return 1;
 
-    FILE *file = fopen(argv[1], "r");
-    if (!file) return 1;
+    FILE *in = fopen(argv[1], "rb");
+    FILE *out = fopen(argv[2], "wb");
+    if (!in || !out) return 1;
 
-    fseek(file, 0, SEEK_END);
-    long length = ftell(file);
-    rewind(file);
+    encrypt(in, out);
 
-    char *buffer = malloc(length + 1);
-    if (!buffer) return 1;
-
-    fread(buffer, 1, length, file);
-    buffer[length] = '\0';
-    fclose(file);
-
-    printf("%s", buffer);
-
-    free(buffer);
+    fclose(in);
+    fclose(out);
     return 0;
 }
