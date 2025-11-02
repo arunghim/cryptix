acc1079cb9bba60436eed1f9c0f4bdf602ff019e
Arun Ghimire <dev.arunghimire@gmail.com>
2025-11-02 15:08:59 -0500

    Fix: Lossless Encryption

--- encrypt.c
+++ encrypt.c
@@ -1,40 +1,33 @@
 #include "headers.h"
 
-#define BLOCK_SIZE 8
-
-void encrypt(FILE *in, FILE *out) {
-    unsigned char block[BLOCK_SIZE];
+void encrypt(FILE *in, FILE *out)
+{
+    unsigned char buf[8];
     size_t n;
-
-    while ((n = fread(block, 1, BLOCK_SIZE, in)) > 0) {
-        if (n < BLOCK_SIZE) {
-            for (size_t i = 0; i < n; ++i)
-                fputc((block[i] % 95) + 32, out);
+    while ((n = fread(buf, 1, 8, in)) > 0)
+    {
+        if (n < 8)
+        {
+            fwrite(buf, 1, n, out);
             break;
         }
 
-        unsigned char enc[BLOCK_SIZE - 1];
-        unsigned char src = block[7];
+        unsigned char src = buf[7];
 
-        int h[8];
-        for (int i = 1; i <= 7; ++i)
-            h[i] = (src >> (7 - i)) & 1;
-
-        for (int i = 0; i < 7; ++i) {
+        for (int i = 0; i < 7; ++i)
+        {
             int bitPos = 7 - i;
-            int bitVal = h[7 - i];
-            unsigned char b = block[i];
-            unsigned char result = 0;
-
-            for (int bit = 7; bit > bitPos; --bit)
-                result |= ((b >> (bit - 1)) & 1) << bit;
-            result |= (bitVal << bitPos);
-            for (int bit = bitPos - 1; bit >= 0; --bit)
-                result |= ((b >> bit) & 1) << bit;
-
-            enc[i] = (result % 95) + 32;
+            int srcBitPos = 6 - i;
+            unsigned char a_bit = (buf[i] >> bitPos) & 1;
+            unsigned char b_bit = (src >> srcBitPos) & 1;
+            if (a_bit != b_bit)
+            {
+                buf[i] ^= (1u << bitPos);
+                src ^= (1u << srcBitPos);
+            }
         }
 
-        fwrite(enc, 1, 7, out);
+        buf[7] = src;
+        fwrite(buf, 1, 8, out);
     }
 }
