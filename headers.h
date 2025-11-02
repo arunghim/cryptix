32cc901500196e43658e4a57b8709fe0b0223902
Arun Ghimire <dev.arunghimire@gmail.com>
2025-11-02 15:23:14 -0500

    Update: Headers + Main File

--- headers.h
+++ headers.h
@@ -2,7 +2,9 @@
 #define HEADERS_H
 
 #include <stdio.h>
+#include <stdint.h>
 
 void encrypt(FILE *in, FILE *out);
+void decrypt(FILE *in, FILE *out);
 
 #endif
--- main.c
+++ main.c
@@ -1,23 +1,38 @@
 #include "headers.h"
 #include <string.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
     if (argc != 4)
+    {
+        printf("Usage: %s <encrypt|decrypt> input.txt output\n", argv[0]);
         return 1;
+    }
 
     FILE *in = fopen(argv[2], "rb");
     if (!in)
+    {
+        perror("Failed to open input file");
         return 1;
+    }
+
     FILE *out = fopen(argv[3], "wb");
     if (!out)
     {
+        perror("Failed to open output file");
         fclose(in);
         return 1;
     }
 
     if (strcmp(argv[1], "encrypt") == 0)
         encrypt(in, out);
+    else if (strcmp(argv[1], "decrypt") == 0)
+        decrypt(in, out);
+    else
+    {
+        printf("Invalid mode: %s. Use 'encrypt' or 'decrypt'.\n", argv[1]);
+    }
 
     fclose(in);
     fclose(out);
